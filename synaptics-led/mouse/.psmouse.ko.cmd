6a9af15a02638d170f06941a2e25a0baeeab5ced
vantu5z <vantu5z@mail.ru>
2021-03-18 14:27:30 +0300

    remove build files

--- synaptics-led/mouse/.psmouse.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vantu5z/git/synaptics-led/synaptics-led/mouse/psmouse.ko := ld -r -m elf_x86_64 --build-id=sha1  -T scripts/module.lds -o /home/vantu5z/git/synaptics-led/synaptics-led/mouse/psmouse.ko /home/vantu5z/git/synaptics-led/synaptics-led/mouse/psmouse.o /home/vantu5z/git/synaptics-led/synaptics-led/mouse/psmouse.mod.o;  true
